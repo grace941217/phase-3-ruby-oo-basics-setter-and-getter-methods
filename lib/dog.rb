83d155b08ec1ebe90c4198c1c227d48afb63fd07
Grace <grace941217@gmail.com>
2022-10-31 22:56:20 -0400

    commit tree 7e01a22c5e047ca6c3e1c9d13e46a4e7cea65229

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    attr_accessor :breed
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
 
 end
+
+fido = Dog.new
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-
+    attr_reader :name
+    attr_writer :name
+    attr_accessor :job
 end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
+beyonce.job = "Singer"
